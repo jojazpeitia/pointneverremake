e15d0b896d2644a59753c83ec21d6c5ab2c06bcd
jojazpeitia <jojazpeitia@gmail.com>
2024-03-26 13:50:50 -0500

    remade image background animation + text styling

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <div className="chat-container">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nesciunt in dicta recusandae atque, error facere provident maiores odio odit magni nobis tempore iure soluta at delectus necessitatibus totam voluptate neque ex!</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
